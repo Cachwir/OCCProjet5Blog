f4c0f59dfbf21bb2ab1203f52f80ba6a8d5373df
cachwir <cachwir@gmx.com>
2017-06-19 16:20:07 +0200

    removed the debug forced mode

--- web/index.php
+++ web/index.php
@@ -3,9 +3,6 @@
 use lib\Config;
 use src\app\MyApp;
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../lib/functions.php';
 require_once __DIR__.'/../src/config/config.php';
